f5e2ccaaf273c987eb1c183e015109ec04096a0e
Ireal <pesotskygoscha@yandex.ru>
2022-05-13 23:23:23 +0300

    final commit for this branch

--- shopDjango/mainapp/apps.py
+++ shopDjango/mainapp/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class MainappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mainapp'
+    verbose_name = _('User information')
 
     def ready(self):
         import mainapp.signals
--- shopDjango/order/apps.py
+++ shopDjango/order/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class OrderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'order'
+    verbose_name = _('Orders')
 
     # def ready(self):
     #     import order.signals
